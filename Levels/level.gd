6208dc0f5ff77deebae4b51274c33b4bf6f248b8
Simon Zheng <simz97@outlook.com>
2023-10-13 00:44:31 -0400

    Implement basic fruit dropping mechanic

--- Levels/level.gd
+++ Levels/level.gd
@@ -4,24 +4,36 @@ extends Node2D
 var fruit_scene := preload("res://fruit.tscn")
 
 var player: CharacterBody2D
-var fruit: RigidBody2D
+var current_fruit: Sprite2D
+#var next_fruit: RigidBody2D
+var fruits: Array
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = self.get_node("Player")
-	fruit = fruit_scene.instantiate()
-	add_child(fruit)
+	current_fruit = self.get_node("Fruit")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	fruit.position = Vector2(player.position.x, player.position.y + 25)
-	if Input.is_action_just_released("ui_accept"):
-		# Drop current fruit
-		fruit.gravity_scale = 1.
-		
-		# Generate new fruit
-		fruit = fruit_scene.instantiate()
-		add_child(fruit)
-		fruit.position = Vector2(player.position.x, player.position.y + 25)
+func _process(_delta):
+	current_fruit.position = Vector2(player.position.x - 25, player.position.y + 25)
+
+
+func _physics_process(_delta):
+	pass
+
+
+func _input(event):
+	if event.is_action_released("release_fruit"):
+		drop_fruit()
+
+
+func drop_fruit():
+	gen_fruit()
+
+
+func gen_fruit():
+	var fruit := fruit_scene.instantiate()
+	add_child(fruit)
+	fruit.global_transform.origin = Vector2(player.position.x - 25, player.position.y + 25)
--- fruit.gd
+++ fruit.gd
@@ -1,22 +1,23 @@
 extends RigidBody2D
 
 
-const SPEED = 100.0
-const FALL_VELOCITY = -400.0
+#const SPEED = 100.0
+#const FALL_VELOCITY = -400.0
+
+var collided := false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gravity_scale = 0.
+	body_shape_entered.connect(get_parent().gen_fruit)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_just_released("ui_accept"):
-		gravity_scale = 1.
+	pass
 
 
 func _physics_process(delta):
@@ -28,3 +29,8 @@ func _physics_process(delta):
 	# Handle Drop.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = FALL_VELOCITY
+
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print(collided)
+	collided = true
